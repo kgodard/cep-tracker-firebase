c6e40fe8aac12ca3f1ee1eb0f51556e8766e2d17
Kim Godard <kim.godard@itron.com>
2019-06-06 18:10:29 -0600

    Add: Display actual ADS story data for sprint!

--- sprint_display.rb
+++ sprint_display.rb
@@ -15,8 +15,7 @@ class SprintDisplay
     EventDisplay.new(events: sprint_events).render
     puts
     puts "Finished stories for sprint ending #{sprint_end}:"
-    report_rule
-    EventDisplay.new(events: uniq_finished_events, with_points: false).render
+    display_stories
     puts
     puts "Sprint Metrics for sprint ending #{sprint_end}:"
     report_rule
@@ -29,7 +28,22 @@ class SprintDisplay
 
   private
 
+  def display_stories
+    sprint.stories.each do |story|
+      report_rule
+      puts "##{story.tracker_id.ljust(9)} | #{trunc(story.title, 65)}"
+      puts "#{story.type.ljust(10)} | #{story.area.ljust(9)} | #{story.iteration.ljust(11)}" +
+        "| #{story.developer.ljust(16)} | %2.2f" % story.points
+    end
+    report_rule
+  end
+
+  def trunc(txt, chars)
+    dotdot = txt.length > chars ? '...' : ''
+    txt[0,chars] + dotdot
+  end
+
   def report_rule
-    puts "----------------------------------------------------------------------"
+    puts "--------------------------------------------------------------------------------"
   end
 end
