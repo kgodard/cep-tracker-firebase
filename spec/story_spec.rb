ad18eb14f4a77224187a05b05212ba51a5105a89
Kim Godard <kim.godard@itron.com>
2019-07-01 15:02:41 -0600

    Fix: Story spec

--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../story.rb'
+require_relative '../ads_story.rb'
 
 describe Story do
 
@@ -25,24 +26,36 @@ describe Story do
     ]
   }
 
+  let(:firebase_event) {
+    double("firebase_event", search: events)
+  }
+
+  let(:ads_story_double) {
+    class_double("AdsStory").
+      as_stubbed_const(:transfer_nested_constants => true)
+  }
+
   subject {
-    Story.new(events: events, developer: developer_name)
+    Story.new(event: events.last, firebase_event: firebase_event)
   }
 
   describe "#points" do
     it "returns the points of the start event" do
+      expect(ads_story_double).to receive(:new).with(id: events.last["tracker_id"])
       expect(subject.points).to eq start_points.to_f
     end
   end
 
   describe "#cycle_hours" do
     it "equals 48 hours" do
+      expect(ads_story_double).to receive(:new).with(id: events.last["tracker_id"])
       expect(subject.cycle_hours).to eq 48.0
     end
   end
 
   describe "#contains_reject?" do
     it "is false" do
+      expect(ads_story_double).to receive(:new).with(id: events.last["tracker_id"])
       expect(subject.contains_reject?).to be false
     end
   end
